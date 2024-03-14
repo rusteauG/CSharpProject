37c01cae588f97fbbd392af24877f0d912ab2853
RusteauN <rrusteau#gmail.com>
2024-03-14 20:30:29 +0400

    day17

---
+++ ArrayEmpClass.cs
@@ -0,0 +1,237 @@
+//Array with Class
+using BethanysPieShopHRM.HR;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 29.23, EmployeeType.Manager);
+
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 21), 15.34, EmployeeType.Research);
+
+Employee mary = new Employee("Mary", "Jones", "mary@snowball.be", new DateTime(1965, 1, 16), 30, EmployeeType.Manager);
+
+Employee bobJunior = new Employee("Bob", "Spencer", "bob@snowball.be", new DateTime(1988, 1, 23), 17, EmployeeType.Research);
+
+Employee kevin = new Employee("Kevin", "Marks", "kevin@snowball.be", new DateTime(1953, 12, 12), 10, EmployeeType.StoreManager);
+
+Employee kate = new Employee("Kate", "Greggs", "kate@snowball.be", new DateTime(1993, 8, 8), 10, EmployeeType.StoreManager);
+
+Employee kim = new Employee("Kim", "Jacobs", "kim@snowball.be", new DateTime(1975, 5, 14), 22, EmployeeType.StoreManager);
+
+Employee[] employees = new Employee[7] { bethany, george, mary, bobJunior, kevin, kate, kim };
+foreach (Employee employee in employees)
+{
+    employee.DisplayEmployeeDetails();
+    var numberOfHoursWorked = new Random().Next(25);
+    employee.PerformWork(numberOfHoursWorked);
+    employee.ReceiveWage();
+
+}
+
+
+//bethany.PerformWork();
+//bethany.PerformWork();
+//bethany.PerformWork(5);
+//bethany.PerformWork();
+
+//double receivedWageBethany = bethany.ReceiveWage(true);
+//Console.WriteLine($"Wage paid (message from Program): {receivedWageBethany}");
+
+////bethany.firstName = "John";
+////bethany.hourlyRate = 10;
+
+//bethany.DisplayEmployeeDetails();
+//bethany.PerformWork();
+//bethany.PerformWork(12);
+//bethany.PerformWork();
+//bethany.ReceiveWage();
+
+
+//Console.WriteLine("Creating an employee");
+//Console.WriteLine("--------------------\n");
+
+//george.DisplayEmployeeDetails();
+//george.PerformWork();
+//george.PerformWork();
+//george.PerformWork(3);
+//george.PerformWork();
+//george.PerformWork(8);
+
+//var receivedWageGeorge = george.ReceiveWage(true);
+
+
+//int a = 42;
+//int aCopy = a;
+//aCopy = 100;
+//Console.WriteLine($"Value of a: {a} and value of copy of a: {aCopy}");
+
+//Employee testEmployee = bethany;
+//testEmployee.firstName = "Gill";
+
+//testEmployee.DisplayEmployeeDetails();
+//bethany.DisplayEmployeeDetails();
+
+//bethany.PerformWork(25);
+
+//int minimumBonus = 100;
+//int receivedBonus = bethany.CalculateBonus(minimumBonus);
+//Console.WriteLine($"The minimum bonus is {minimumBonus} and {bethany.firstName} has received a bonus of {receivedBonus}");
+
+
+//int minimumBonus = 100;
+//int bonusTax = 0;
+//int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, ref bonusTax);
+//Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus }");
+
+int minimumBonus = 100;
+int bonusTax;
+int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, out bonusTax);
+Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
+
+
+WorkTask task;
+task.description = "Bake delicious pies";
+task.hours = 3;
+task.PerformWorkTask();
+
+
+
+//Enum type Seperate
+
+namespace BethanysPieShopHRM.HR
+{
+    enum EmployeeType
+    {
+        Sales,
+        Manager,
+        Research,
+        StoreManager
+    }
+}
+
+
+
+//Employee.cs  Class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.HR
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+        public EmployeeType employeeType;
+
+        public DateTime birthDay;
+
+        public static double taxRate = 0.129;
+        
+        const int minimalHoursWorkedUnit = 1;
+
+        public Employee(string first, string last, string em, DateTime bd, double rate, EmployeeType empType)
+        {
+            firstName = first;
+            lastName = last;
+            email = em;
+            birthDay = bd;
+            hourlyRate = rate;
+            employeeType = empType;
+        }
+
+        
+
+        public void PerformWork()
+        {
+            PerformWork(minimalHoursWorkedUnit);
+        }
+
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+
+        public int CalculateBonus(int bonus)
+        {
+
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;
+        }
+
+        //public int CalculateBonusAndBonusTax(int bonus, ref int bonusTax)
+        //{
+
+        //    if (numberOfHoursWorked > 10)
+        //        bonus *= 2;
+
+        //    if (bonus >= 200)
+        //    {
+        //        bonusTax = bonus / 10;
+        //        bonus -= bonusTax;
+        //    }
+
+        //    Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+        //    return bonus;
+        //}
+
+        public int CalculateBonusAndBonusTax(int bonus, out int bonusTax)
+        {
+            bonusTax = 0;
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+
+            if (bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }
+
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+            return bonus;
+        }
+
+
+        public double ReceiveWage(bool resetHours = true)
+        {
+            double wageBeforeTax = 0.0;
+            if (employeeType == EmployeeType.Manager)
+            {
+                Console.WriteLine($"An extra was added to the wage since {firstName} is a manager!");
+                wageBeforeTax = (double)(numberOfHoursWorked * hourlyRate * 1.25);
+            }
+            else
+            {
+                wageBeforeTax = (numberOfHoursWorked * hourlyRate);
+            }
+
+            double taxAmount = wageBeforeTax * taxRate;
+            double wage = wageBeforeTax - taxAmount;
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of After tax:{taxRate*100}% Wage:{Math.Round(wage,2)} for {numberOfHoursWorked} hour(s) of work.\nBeforeTax Sal :{wageBeforeTax:0.00}");
+
+            if (resetHours)
+                numberOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n" +
+                $"\nTaxRate: {taxRate}");
+        }
+    }
+}
